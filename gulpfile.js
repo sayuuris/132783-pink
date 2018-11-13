58ee891d7744f602922a4efe9104c64f032b0577
sayuris <oleq.artyomov@gmail.com>
2018-11-13 18:49:39 +0400

    gulpfile source maps

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,19 @@ var postcss = require("gulp-postcss");
 var plumber = require("gulp-plumber");
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
+var sourcemaps = require("gulp-sourcemaps");
 var server = require("browser-sync").create();
 
 gulp.task("css", function() {
     return gulp.src("source/less/style.less")
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(less())
         .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest("source/css"))
         .pipe(csso())
         .pipe(rename("style.min.css"))
+        .pipe(sourcemaps.write("../maps"))
         .pipe(gulp.dest("source/css"))
         .pipe(server.stream());
 });
@@ -26,6 +29,6 @@ gulp.task("server", function() {
         server:"source/"
     });
 gulp.watch("source/less/**/*.less", gulp.series("css"));
-gulp.watch("source/*.html").on("change", server.reload());
+gulp.watch("source/*.html").on("change", server.reload);
 });
 gulp.task("start", gulp.series("css", "server"));
